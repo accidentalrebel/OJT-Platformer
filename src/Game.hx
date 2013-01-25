8b5e17721f475d301c988730dc8616eba96c282b
Karlo <Karlo@localhost>
2013-01-25 18:38:28 +0800

    Made the player spritesheet into a long one Player now has idle, run, and jump animations

--- src/Game.hx
+++ src/Game.hx
@@ -20,9 +20,10 @@ class Game extends JKGame
 		bgLayer = new JKLayer(stage);
 		fgLayer = new JKLayer(stage);		
 		
-		map = new Map(bgLayer);		
+		//map = new Map(bgLayer);		
 		player = new Player(0, 0, 40, 50, "img/player.png", true, fgLayer);
-		player.addAnimation("idle", [ 0, 1, 2, 3], 150 );
-		player.play("idle");
+		//player.play("idle");
+		//player.play("run");
+		player.play("jump");
 	}	
 }
--- src/Player.hx
+++ src/Player.hx
@@ -13,5 +13,9 @@ class Player extends JKSprite
 		, ?FrameHeight : Float, ?graphicFileLocation : String, IsAnimated : Bool = false, ?theLayer : DisplayObjectContainer ) 
 	{
 		super(xPos, yPos, FrameWidth, FrameHeight, graphicFileLocation, IsAnimated, theLayer);
+		
+		addAnimation("idle", [ 0, 1, 2, 3], 150 );
+		addAnimation("run", [4, 5, 6, 7, 8, 9, 10, 11], 150 );
+		addAnimation("jump", [ 12, 13, 14, 15, 16, 17, 18], 150 );		
 	}	
 }
