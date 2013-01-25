4b99c8e57955cfa8f577838c939f2eaa6930b7e6
Karlo <Karlo@localhost>
2013-01-25 18:01:21 +0800

    Player sprite can now follow an animation speed

--- src/Game.hx
+++ src/Game.hx
@@ -22,7 +22,7 @@ class Game extends JKGame
 		
 		map = new Map(bgLayer);		
 		player = new Player(0, 0, 40, 50, "img/player.png", true, fgLayer);
-		player.addAnimation("idle", [ 0, 1, 2, 3] );
+		player.addAnimation("idle", [ 0, 1, 2, 3], 150 );
 		player.play("idle");
 	}	
 }
