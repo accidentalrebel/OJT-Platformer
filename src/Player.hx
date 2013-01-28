2a464d97583e95d54e4ef063e044d8b05a16e4e6
Karlo <Karlo@localhost>
2013-01-28 10:06:34 +0800

    Pressing the right button moves the player to the right

--- src/Player.hx
+++ src/Player.hx
@@ -11,6 +11,7 @@ import nme.Lib;
 
 class Player extends JKSprite
 {
+	var movementSpeed : Float = 3;
 
 	public function new( xPos : Float = 0, yPos : Float = 0, ?FrameWidth : Float
 		, ?FrameHeight : Float, ?graphicFileLocation : String, IsAnimated : Bool = false, ?theLayer : DisplayObjectContainer ) 
@@ -18,8 +19,10 @@ class Player extends JKSprite
 		super(xPos, yPos, FrameWidth, FrameHeight, graphicFileLocation, IsAnimated, theLayer);
 		
 		addAnimation("idle", [ 0, 1, 2, 3], 150 );
-		addAnimation("run", [4, 5, 6, 7, 8, 9, 10, 11], 150 );
+		addAnimation("run", [4, 5, 6, 7, 8, 9, 10, 11], 100 );
 		addAnimation("jump", [ 12, 13, 14, 15, 16, 17, 18], 150 );		
+		
+		play("idle");
 	}	
 	
 	override private function update():Dynamic 
@@ -42,10 +45,22 @@ class Player extends JKSprite
 		else if ( Registry.game.keyboard.checkIfKeyPressed("d") )
 		{
 			Lib.trace("d is pressed");
+			moveRight();
+		}
+		else if ( Registry.game.keyboard.checkIfKeyReleased("d") )
+		{
+			Lib.trace("d is pressed");
+			play("idle");
 		}
 		else if ( Registry.game.keyboard.checkIfKeyPressed("spacebar") )
 		{
 			Lib.trace("spacebar is pressed");
 		}
+	}
+	
+	function moveRight() : Void
+	{
+		x += movementSpeed;		
+		play("run");
 	}
 }
